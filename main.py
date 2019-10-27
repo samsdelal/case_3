4bb7d78adef6c2f0f1b54a958dc6b019d7645b5a
Ampechture <qadropopishe@yandex.ru>
2019-10-27 22:42:28 +0700

    Wnile_start

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 month = 0
-invest = 0
-percent_month = 0
+invest_start = int(input())
+percent = int(input())
+percent_month = percent / 100
 capital = 0
-year = 0
+years = int(input())
+period = 1
+all_periods = 12
+i = 0
+u_1 = 0
+while u_1 != years * 12:
+    Total_per_month = invest_start * (1 + percent * (period/all_periods)) ** (all_periods * i)
+    summ_month = invest_start * percent
+    print(Total_per_month)
+
+    period += 1
+    i += 1
+    u_1 += 1
