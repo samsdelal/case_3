d2f101230cc84e312679f313ed6b1d267cb09983
Ampechture <qadropopishe@yandex.ru>
2019-10-27 23:12:12 +0700

    Merge branch 'master' of C:\Users\qadro\PycharmProjects\case_3 with conflicts.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ percent = int(input("Процентная ставка (%/мес.):"))
 inveset_river = int(input("Инвестиционные вливания ($/мес.):"))
 percent_month = percent / 100
 capital = 0
+years = int(input())
 period = 1
 all_periods = 12
 i = 0
@@ -12,9 +13,10 @@ u_1 = 0
 
 while u_1 != years * 12 +:
     Total_per_month = invest_start * (1 + percent * (period / all_periods)) + inveset_river
+while u_1 != years * 12:
+    Total_per_month = invest_start * (1 + percent * (period/all_periods))
     summ_month = invest_start * percent
     print(Total_per_month)
-
     period += 1
     i += 1
     u_1 += 1
