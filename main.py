9b40c6eb9200c6c8380f11f83c13b1694cae968d
Ampechture <qadropopishe@yandex.ru>
2019-10-27 23:15:26 +0700

    while_total_with_the_new_year

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ capital = 0
 period = 1
 all_periods = 12
 i = 0
-u_1 = 0
-
-while u_1 != years * 12:
+months = 0
+months_1 = 0
+while months_1 != years * 12:
     Total_per_month = invest_start * (1 + percent * (period / all_periods)) + invest_river             #Нужен вывод
     summ_month = invest_start * percent                                                                #Нужен вывод
 
@@ -18,7 +18,11 @@ while u_1 != years * 12:
 
     period += 1
     i += 1
-    u_1 += 1
+    months_1 += 1
+    if months == 12:
+        months = 0
+        months += 1
+        print('new_year')
 
 
 
