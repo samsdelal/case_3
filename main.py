9d2578d01758fe41099e21b484f665140a875b5a
theMkrv <themkrv@gmail.com>
2019-10-27 22:51:44 +0700

    Love Minak

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 month = 0
-invest_start = int(input())
-percent = int(input())
+years = int(input("Срок размещения капитала (лет):"))
+invest_start = int(input("Начальный капитал ($):"))
+percent = int(input("Процентная ставка (%/мес.):"))
+inveset_river = int(input("Инвестиционные вливания ($/мес.):"))
 percent_month = percent / 100
 capital = 0
-years = int(input())
 period = 1
 all_periods = 12
 i = 0
 u_1 = 0
-while u_1 != years * 12:
-    Total_per_month = invest_start * (1 + percent * (period/all_periods)) ** (all_periods * i)
+
+while u_1 != years * 12 +:
+    Total_per_month = invest_start * (1 + percent * (period / all_periods)) + inveset_river
     summ_month = invest_start * percent
     print(Total_per_month)
 
     period += 1
     i += 1
     u_1 += 1
+
+
+
