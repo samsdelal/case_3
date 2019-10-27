6a8c640236ba00d1e78f1e34af05b1b8e97782b8
theMkrv <themkrv@gmail.com>
2019-10-27 22:58:28 +0700

    Love Minak

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ u_1 = 0
 while u_1 != years * 12:
     Total_per_month = invest_start * (1 + percent * (period / all_periods)) + invest_river
     summ_month = invest_start * percent
+
     print(Total_per_month)
 
     period += 1
     i += 1
     u_1 += 1
+
+
 
 
 
