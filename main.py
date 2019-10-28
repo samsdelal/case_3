f55246c047c6d5fd5d216f9dd6c3bcf428f5bc18
Boris Kuznecov <oralebot@mail.ru>
2019-10-28 22:11:57 +0700

    some add

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import math as m
+import local as lc
+
+MONTH = 1
+CAPITAL = 0
+
 year = int(input("Срок размещения капитала (лет): "))
-i_cap = float(input("Начальный капитал ($): "))
-percent = float(input("Процентная ставка (%/мес.): "))
-investment = float(input("Инвестиционные вливания ($/мес.): "))
+print(lc.TEXT_END)
+capital_start = float(input("Начальный капитал ($): "))
+print(lc.TEXT_END)
+percent_bank = float(input("Процентная ставка (%/мес.): "))
+print(lc.TEXT_END)
+investment_give = float(input("Инвестиционные вливания ($/мес.): "))
+
+for year_print in range(year):
+    month = 0
+    year_print += 1
+    print("")
+    print(year_print, "ГОД / YEAR")
+    print(lc.TEXT_END)
+    print("| month |   основа   | сумма %  |         |")
+    print("| месяц | инвестиций | за месяц | капитал |")
+    print(lc.TEXT_END)
 
 
 
