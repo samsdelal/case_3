dae8de48a1be7b97953c2eb8457d0776c6456e8e
Boris Kuznecov <oralebot@mail.ru>
2019-10-28 21:57:59 +0700

    all changed

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-month = 0
-years = int(input("Срок размещения капитала (лет):"))
-invest_start = int(input("Начальный капитал ($):"))
-percent = int(input("Процентная ставка (%/мес.):"))
-invest_river = int(input("Инвестиционные вливания ($/мес.):"))
-percent_month = percent / 100
-capital = 0
-period = 1
-all_periods = 12
-i = 0
-months = 1
-months_1 = 0
-while months_1 != years * 12:
-    Total_per_month = invest_start * (1 + percent * (period / all_periods)) + invest_river             #Нужен вывод
-    summ_month = Total_per_month * percent                                                                #Нужен вывод
-
-    print(Total_per_month)
-    months += 1
-    period += 1
-    i += 1
-    months_1 += 1
-    if months == 12:
-        months = 0
-        print('new_year')
+import math as m
+year = int(input("Срок размещения капитала (лет): "))
+i_cap = float(input("Начальный капитал ($): "))
+percent = float(input("Процентная ставка (%/мес.): "))
+investment = float(input("Инвестиционные вливания ($/мес.): "))
 
 
 
